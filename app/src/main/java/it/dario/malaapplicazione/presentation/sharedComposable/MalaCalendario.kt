2b6822d6a8b46b5cfaf148200c4338b10add03b9
d.trisconi <dario.trisconi@gmail.com>
2023-05-27 15:25:14 +0200

    non lazy calendar

--- app/src/main/java/it/dario/malaapplicazione/presentation/sharedComposable/MalaCalendario.kt
+++ app/src/main/java/it/dario/malaapplicazione/presentation/sharedComposable/MalaCalendario.kt
@@ -1,18 +1,11 @@
 package it.dario.malaapplicazione.presentation.inserisciDisponibilita.widgets
 
 import android.util.Log
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowColumn
-import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -24,36 +17,104 @@ import java.text.DateFormatSymbols
 import java.time.LocalDate
 import java.util.Calendar
 
-private val MY_DAY_OF_WEEK_TO_CALENDAR = arrayOf(Calendar.MONDAY, Calendar.TUESDAY, Calendar.WEDNESDAY, Calendar.THURSDAY, Calendar.FRIDAY, Calendar.SATURDAY, Calendar.SUNDAY)
-fun DateFormatSymbols.getShortWeekdaysByMyDayOfWeek() = Array(7) { shortWeekdays[MY_DAY_OF_WEEK_TO_CALENDAR[it]] }
+private val MY_DAY_OF_WEEK_TO_CALENDAR = arrayOf(
+    Calendar.MONDAY,
+    Calendar.TUESDAY,
+    Calendar.WEDNESDAY,
+    Calendar.THURSDAY,
+    Calendar.FRIDAY,
+    Calendar.SATURDAY,
+    Calendar.SUNDAY
+)
 
-@OptIn(ExperimentalLayoutApi::class)
+fun DateFormatSymbols.getShortWeekdaysByMyDayOfWeek() =
+    Array(7) { shortWeekdays[MY_DAY_OF_WEEK_TO_CALENDAR[it]] }
+
 @Preview(showBackground = true)
 @Composable
 fun MalaCalendario(
     modifier: Modifier = Modifier,
     startDate: LocalDate = LocalDate.of(2023, 8, 1),
     endDate: LocalDate = LocalDate.of(2023, 9, 4),
-    dayContent: @Composable (LocalDate) -> Unit = { PreviewDay(day = it)},
-    ) {
+    dayContent: @Composable (LocalDate) -> Unit = { PreviewDay(day = it) },
+) {
 
     Log.d(Constants.TAG, "composing MalaCalendario")
 
+    val offsetItems = (0 until startDate.dayOfWeek.value - 1).toList()
+    val columns = 7
+    val days = startDate.rangeTo(endDate).toList()
 
-    val offsetItems = (0 until startDate.dayOfWeek.value-1).toList()
+    //non includo gli headers visto che hanno già la loro riga
+    val itemCount = offsetItems.count() + //+ offset
+            days.count() // + numero giorni
+    val rows = (itemCount / columns)
 
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(7),
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            userScrollEnabled = false
-        ) {
+    var itemInserted = 0
 
-            items(DateFormatSymbols().getShortWeekdaysByMyDayOfWeek()) { HeaderDay(it) }
-            items(offsetItems) { BlankDay() }
-            items(startDate.rangeTo(endDate).toList()) {
-                dayContent(it)
+    Column(modifier = modifier) {
+
+        HeaderRow(modifier = modifier) //headers
+
+        /* prima riga (con offset)*/
+        Row {
+            offsetItems.forEach {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                ) {
+                    BlankDay()
+                }
+            }
+            days.take(columns - offsetItems.count()).forEach {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                ) {
+                    itemInserted++
+                    dayContent(it)
+                }
             }
         }
+
+        //iteminserted = 6
+        /* gestione di tutte le altre righe*/
+        for (rowId in 0 until rows) {
+            val firstIndex = rowId * columns
+
+            Row {
+                for (columnId in 0 until columns) {
+                    val index = firstIndex + columnId
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f)
+                    ) {
+                        if (itemInserted < days.count()) {
+                            dayContent(days[itemInserted++])
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun HeaderRow(
+    modifier: Modifier
+) {
+    Row(modifier = modifier) {
+        DateFormatSymbols().getShortWeekdaysByMyDayOfWeek().forEach {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) { HeaderDay(label = it) }
+        }
+    }
 }
 
 @Composable
@@ -86,9 +147,9 @@ private fun PreviewDay(day: LocalDate) {
         modifier = Modifier.aspectRatio(1f), // This is important for square sizing!
         contentAlignment = Alignment.Center,
     ) {
-            Text(
-                text = day.dayOfMonth.toString(),
-            )
+        Text(
+            text = day.dayOfMonth.toString(),
+        )
     }
 }
 
