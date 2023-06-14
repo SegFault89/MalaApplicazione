632f8f103d0a593b58faacd78c1a5a1cbefe5034
Dario <trisconi.dario@gmail.com>
2023-06-15 00:02:17 +0200

    prima release (test)

--- app/src/main/java/it/dario/malaapplicazione/presentation/home/Home.kt
+++ app/src/main/java/it/dario/malaapplicazione/presentation/home/Home.kt
@@ -3,11 +3,11 @@ package it.dario.malaapplicazione.presentation.home
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -18,8 +18,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.compose.ConstraintLayout
 import it.dario.malaapplicazione.BuildConfig
 import it.dario.malaapplicazione.R
+import it.dario.malaapplicazione.data.datasources.MockDataSource
+import it.dario.malaapplicazione.domain.repositories.DisponibilitaRepository
 import it.dario.malaapplicazione.presentation.MalaViewModel
 import it.dario.malaapplicazione.presentation.home.widgets.HomeButton
 import it.dario.malaapplicazione.presentation.sharedcomposable.BugReportIcon
@@ -35,6 +39,7 @@ import it.dario.malaapplicazione.presentation.theme.MarginSmall
  * @param onNavigateToInserisci funzione per navigare verso la pagina InserisciDisponibilita
  * @param onNavigateToVisualizza funzione per navigare verso la pagina VisualizzaDisponibilita
  * @param onNavigateToDatiFattura funzione per navigare verso la pagina DatiFattura
+ * @param openBug funzione per navigare verso il dialog di apertura segnalazioni
  *
  */
 @OptIn(ExperimentalMaterial3Api::class)
@@ -64,35 +69,74 @@ fun Home(
         ) {
             //Indicatore di caricamento
             MalaProgressIndicator(Modifier.align(Alignment.TopEnd), viewModel.isReady)
-            //Footer
-            Footer(modifier = Modifier.align(Alignment.BottomCenter))
-            Column(
+            ConstraintLayout (
                 modifier = Modifier
                     .fillMaxSize()
-                    .width(IntrinsicSize.Max)
                     .padding(MarginBig),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceEvenly,
             ) {
-                val buttonModifier = Modifier.fillMaxWidth()
-                HomeButton(
-                    modifier = buttonModifier,
-                    label = stringResource(id = R.string.inserisci_disponibilita),
-                    onclick = onNavigateToInserisci,
-                    enabled = viewModel.isReady
+                val (buttons, footer) = createRefs()
+
+                Buttons(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .constrainAs(buttons) {
+                            top.linkTo(parent.top)
+                            bottom.linkTo(footer.top)
+                        },
+                    viewModel = viewModel,
+                    onNavigateToDatiFattura = onNavigateToDatiFattura,
+                    onNavigateToInserisci = onNavigateToInserisci,
+                    onNavigateToVisualizza = onNavigateToVisualizza
                 )
-                HomeButton(
-                    modifier = buttonModifier,
-                    label = stringResource(id = R.string.visualizza_disponibilita),
-                    onclick = onNavigateToVisualizza,
-                    enabled = viewModel.isReady
-                )
-                HomeButton(
-                    modifier = buttonModifier,
-                    label = stringResource(id = R.string.dati_fattura),
-                    onclick = onNavigateToDatiFattura
-                )
+                //Footer
+                Footer(modifier = Modifier.constrainAs(footer){
+                    bottom.linkTo(parent.bottom)
+                })
             }
+        }
+    }
+}
+
+@Composable
+fun Buttons(
+    modifier: Modifier = Modifier,
+    viewModel: MalaViewModel,
+    onNavigateToInserisci: () -> Unit = {},
+    onNavigateToVisualizza: () -> Unit = {},
+    onNavigateToDatiFattura: () -> Unit = {},
+) {
+    val buttonModifier = Modifier.fillMaxWidth()
+    LazyVerticalGrid(
+        modifier = modifier,
+        columns = GridCells.Fixed(1),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        item {
+            HomeButton(
+                modifier = buttonModifier,
+                label = stringResource(id = R.string.inserisci_disponibilita),
+                description = stringResource(id = R.string.inserisci_disponibilita_description),
+                onclick = onNavigateToInserisci,
+                enabled = viewModel.isReady
+            )
+        }
+        item {
+            HomeButton(
+                modifier = buttonModifier,
+                label = stringResource(id = R.string.visualizza_disponibilita),
+                description = stringResource(id = R.string.visualizza_disponibilita_description),
+                onclick = onNavigateToVisualizza,
+                enabled = viewModel.isReady
+            )
+        }
+        item {
+            HomeButton(
+                modifier = buttonModifier,
+                label = stringResource(id = R.string.dati_fattura),
+                description = stringResource(id = R.string.dati_fattura_description),
+                onclick = onNavigateToDatiFattura
+            )
         }
     }
 }
@@ -119,10 +163,15 @@ fun Footer(modifier: Modifier) {
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(
-                    bottom = MarginNormal,
                     start = MarginNormal,
                     end = MarginSmall
                 )
         )
     }
+}
+
+@Preview(showSystemUi = true, device = "spec:width=300dp,height=841dp,dpi=480")
+@Composable
+fun Preview() {
+    Home(viewModel = MalaViewModel(DisponibilitaRepository(MockDataSource())))
 }
